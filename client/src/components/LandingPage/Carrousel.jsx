ed4841e23262b3290ec9ad2a308ca4796017630f
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-04-19 16:42:19 -0300

    Terminada la Landing Page

---
+++ client/src/components/LandingPage/Carrousel.jsx
@@ -0,0 +1,39 @@
+import {useDispatch, useSelector } from 'react-redux'
+import styles from './LandingPage.module.css'
+import { useState, useEffect } from 'react'
+import { getExamples } from "../../actions";
+
+const Carrousel = () =>{
+    const dispatch = useDispatch()
+  const recipes = useSelector((state) => state.recipes)
+  useEffect(()=>{
+    dispatch(getExamples())
+  },[dispatch])
+  
+  const [actualImage, setactualImage] = useState(0);
+  const images = recipes.map(({title, image}, index)=> {
+    return <>
+    <div className={actualImage === index ? `${styles.carrousel} ${styles.active}` : styles.carrousel }>
+        { actualImage === index &&( <>
+        <h2 className={styles.titleCarrousel}>{title}</h2>
+        <img key={index} src={image} alt={title} className={styles.imageCarrousel} />
+        </>)}
+    </div>
+    </>} )
+  const totalImages = images?.length
+  
+  const nextImage = () =>{
+    setactualImage(actualImage === totalImages - 1 ? 0 : actualImage + 1)
+  }
+  const prevImage = () =>{
+    setactualImage(actualImage === 0 ? totalImages - 1 : actualImage - 1)
+  }
+    return(
+        <div>
+            <button onClick={prevImage} className={styles.prevButtonCarrousel}>←</button>
+        {images}
+        <button onClick={nextImage} className={styles.nextButtonCarrousel}>→</button>
+        </div>
+    )
+}
+export default Carrousel
--- client/src/components/LandingPage/LandingPage.jsx
+++ client/src/components/LandingPage/LandingPage.jsx
@@ -1,41 +1,17 @@
 import {Link} from 'react-router-dom'
 import styles from './LandingPage.module.css'
-import {useDispatch, useSelector } from 'react-redux'
-import { useState, useEffect } from 'react'
-import { getExamples } from "../../actions";
+import Carrousel from './Carrousel';
 
 const LandingPage = () =>{
-  const dispatch = useDispatch()
-  const recipes = useSelector((state) => state.recipes)
-  useEffect(()=>{
-    dispatch(getExamples())
-  },[dispatch])
   
-  const [actualImage, setactualImage] = useState(0);
-  const images = recipes.map(({title, image}, index)=> {
-    return <>
-    <div className={actualImage === index ? `${styles.carrousel} ${styles.active}` : styles.carrousel }>{ actualImage === index &&( <><h2 className={styles.titleCarrousel}>{title}</h2><img key={index} src={image} alt={title} className={styles.imageCarrousel} /></>)}
-    </div>
-    </>} )
-  const totalImages = images?.length
-  
-  const nextImage = () =>{
-    setactualImage(actualImage === totalImages - 1 ? 0 : actualImage + 1)
-  }
-  const prevImage = () =>{
-    setactualImage(actualImage === 0 ? totalImages - 1 : actualImage - 1)
-  }
   
     return(
         <> <div className={styles.goHomeDiv}><Link to='/home' style={{textDecoration: 'none', color: 'black'}}><p className={styles.goHomeP}>Click here to go to the Home page</p></Link></div>
         <div className={styles.firstDiv}></div>
-        <div className={styles.secondDiv}>ABOUT</div>
-        <div className={styles.thirdDiv}>CARROUSEL
-        <button onClick={prevImage} className={styles.prevButtonCarrousel}>←</button>
-        {images}
-        <button onClick={nextImage} className={styles.nextButtonCarrousel}>→</button>
-        </div>
-        <div className={styles.fourthDiv}>Copyright 2023</div></>
+        <div className={styles.secondDiv}><div className={styles.welcome}>Hi there! Welcome to Kitchen Cabinet</div><div className={styles.about}>  Kitchen Cabinet is a one-stop destination for all cooking enthusiasts. With a vast collection of recipes for a variety of dishes, this website makes it easy for you to find the perfect recipe for any occasion. Whether you are a seasoned chef or a beginner in the kitchen, Kitchen Cabinet offers a user-friendly interface that makes browsing and searching for recipes a breeze. Moreover, the website provides a unique feature where users can create and store their own recipes in their personal cabinet. This allows you to easily access your favorite recipes whenever you need them. With Kitchen Cabinet, cooking has never been so convenient and enjoyable.</div></div>
+        <div className={styles.thirdDiv}></div>
+        <Carrousel />
+        <div className={styles.fourthDiv}>Copyright 2023 | Kitchen Cabinet <div>Developed by: Rodrigo Villarreal</div></div></>
     )
     }
     export default LandingPage;
