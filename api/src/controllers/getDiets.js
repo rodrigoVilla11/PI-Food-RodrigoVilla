7223653942f55ae97969d63fc97857c0c8c0c7ad
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-04-28 10:25:10 -0300

    Areglando bug diets

--- api/src/controllers/getDiets.js
+++ api/src/controllers/getDiets.js
@@ -8,16 +8,18 @@ const getDiets = async (req, res) => {
     const dietsApi = await axios.get(
       `https://api.spoonacular.com/recipes/complexSearch/?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
     );
+    const diets = [];
     const diet = dietsApi.data.results.flatMap((el) => el.diets);
     dietsApi.data.results.forEach((el) => {
       if (el.vegetarian && !diet.includes("vegetarian"))
         diet.push("vegetarian");
     });
-    const dietFilter = diet.filter((el, index, arr) => {
-      return index === arr.indexOf(el);
+
+    diet.forEach((el) => {
+      if (!diets.includes(el)) diets.push(el);
     });
-    console.log(dietFilter);
-    dietFilter.forEach(async (el) => {
+    console.log(diets);
+    diets.forEach(async (el) => {
       await Diet.findOrCreate({ where: { name: el } });
     });
     const allDiets = await Diet.findAll();
