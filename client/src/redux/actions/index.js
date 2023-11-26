3a3f781898fca80e5c7a16c53dd9f1a02ddbaec4
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-11-26 15:31:46 -0300

    AGREGADO ENV DEPLOY

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+require("dotenv").config();
+const { DEPLOY_BACK } = process.env;
+
 export const GET_RECIPES = "GET_RECIPES";
 export const GET_DETAIL = "GET_DETAIL";
 export const FILTER_BY_DIET = "FILTER_BY_DIET";
@@ -10,89 +13,89 @@ export const GET_DIETS = "GET_DIETS";
 export const GET_EXAMPLES = "GET_EXAMPLES";
 
 export function getRecipes() {
-  return async function (dispatch) {
-    const response = await axios.get("/recipes");
-    return dispatch({
-      type: GET_RECIPES,
-      payload: response.data,
-    });
-  };
+	return async function (dispatch) {
+		const response = await axios.get(DEPLOY_BACK + "/recipes");
+		return dispatch({
+			type: GET_RECIPES,
+			payload: response.data,
+		});
+	};
 }
 
 export function getRecipesById(id) {
-  return async function (dispatch) {
-    const response = await axios.get(`/recipes/${id}`);
-    return dispatch({
-      type: GET_DETAIL,
-      payload: response.data,
-    });
-  };
+	return async function (dispatch) {
+		const response = await axios.get(DEPLOY_BACK + `/recipes/${id}`);
+		return dispatch({
+			type: GET_DETAIL,
+			payload: response.data,
+		});
+	};
 }
 
 export function getRecipesByName(name) {
-  return async function (dispatch) {
-    try {
-      const response = await axios.get(`/recipes?name=` + name);
-      return dispatch({
-        type: GET_RECIPES_BY_NAME,
-        payload: response.data,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
+	return async function (dispatch) {
+		try {
+			const response = await axios.get(DEPLOY_BACK + `/recipes?name=` + name);
+			return dispatch({
+				type: GET_RECIPES_BY_NAME,
+				payload: response.data,
+			});
+		} catch (error) {
+			console.log(error);
+		}
+	};
 }
 export function filterRecipesByDiets(payload) {
-  return {
-    type: FILTER_BY_DIET,
-    payload,
-  };
+	return {
+		type: FILTER_BY_DIET,
+		payload,
+	};
 }
 
 export function filterRecipesByCreator(payload) {
-  return {
-    type: FILTER_BY_CREATOR,
-    payload,
-  };
+	return {
+		type: FILTER_BY_CREATOR,
+		payload,
+	};
 }
 
 export function orderByName(payload) {
-  return {
-    type: ORDER_BY_NAME,
-    payload,
-  };
+	return {
+		type: ORDER_BY_NAME,
+		payload,
+	};
 }
 
 export function orderByHealtScore(payload) {
-  return {
-    type: ORDER_BY_HEALTSCORE,
-    payload,
-  };
+	return {
+		type: ORDER_BY_HEALTSCORE,
+		payload,
+	};
 }
 
 export function getDiets() {
-  return async function (dispatch) {
-    const response = await axios.get("/diets");
-    return dispatch({
-      type: GET_DIETS,
-      payload: response.data,
-    });
-  };
+	return async function (dispatch) {
+		const response = await axios.get(DEPLOY_BACK + "/diets");
+		return dispatch({
+			type: GET_DIETS,
+			payload: response.data,
+		});
+	};
 }
 
 export function postRecipe(payload) {
-  return async function (dispatch) {
-    const response = await axios.post("/recipes", payload);
-    return response;
-  };
+	return async function (dispatch) {
+		const response = await axios.post(DEPLOY_BACK + "/recipes", payload);
+		return response;
+	};
 }
 
 export function getExamples() {
-  return async function (dispatch) {
-    const response = await axios.get("/examplesLandingPage");
-    return dispatch({
-      type: GET_EXAMPLES,
-      payload: response.data,
-    });
-  };
+	return async function (dispatch) {
+		const response = await axios.get(DEPLOY_BACK + "/examplesLandingPage");
+		return dispatch({
+			type: GET_EXAMPLES,
+			payload: response.data,
+		});
+	};
 }
