4f00c9161aae13d0a49bf3763fc6fcf0536b54e8
villaRodrigo11 <villarrealrodrigo.n@gmail.com>
2023-11-26 19:22:16 -0300

    fixed crypto problem

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import store from "./redux/store";
-import "crypto";
 import axios from "axios";
 
 const DEPLOY_BACK = "https://pi-food-back-edzn.onrender.com";
